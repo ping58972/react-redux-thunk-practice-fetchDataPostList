6419d4f5c0bc12724407f03d8baa1d58bb8db02a
ping58972 <ping58972@gmail.com>
2019-05-19 01:46:46 -0500

    Updates

---
+++ precache-manifest.112a4eaaa984e32ef20a83ae80c5ff26.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a9b5f00c5fe8a5067e8f1c35148914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4226020cc7d04bb2f877",
+    "url": "/static/js/2.6a70704a.chunk.js"
+  },
+  {
+    "revision": "3ac95bba67ac7a4af87c",
+    "url": "/static/js/main.6b0672bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
